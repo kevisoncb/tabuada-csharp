229c306e83cce3dcb7a122e3d9d6bd582911a624
Kevison Cardoso <kevisonc.brandes@gmail.com>
2022-08-02 13:39:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tabuada
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int formula, contador, numero;
+
+        Console.WriteLine("Digite o número da tabuada.");
+        numero = int.Parse(Console.ReadLine());
+
+        for(contador = 1; contador <=10; ++contador)
+        {
+            formula = numero * contador;
+            Console.WriteLine(numero + "X" + contador + "=" + formula);
+        }
+        Console.ReadKey();
+    }   
+  }
+}
